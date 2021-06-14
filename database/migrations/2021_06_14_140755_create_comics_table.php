06c8e93e40895fc573f0c789a341a0137cdc058d
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-06-14 16:47:11 +0200

    create seeder

--- database/migrations/2021_06_14_140755_create_comics_table.php
+++ database/migrations/2021_06_14_140755_create_comics_table.php
@@ -15,6 +15,14 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->string('slug');
+            $table->string('series', 30);
+            $table->float('price',4,2)->unsigned();
+            $table->string('image', 30);
+            $table->string('type', 30);
+            $table->date('sale_date');
+            $table->text('description');
             $table->timestamps();
         });
     }
