25079296241bb07f0b971054c5a196016b4f692a
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-06-16 09:15:35 +0200

    add form input

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,7 +5,7 @@
         <h1 class="mb-5">Create New Comics</h1>
         <div class="row">
             <div class="col-md-8 offset-md-2">
-                <form action="{{ Route('comics.store')}}" method="POST">
+                <form action="{{ route('comics.store')}}" method="POST">
                     @csrf
                     @method('POST')
 
@@ -15,6 +15,36 @@
                         <input type="text" class="form-control" id="title" name="title" placeholder=""> 
                     
                     </div>
+
+
+                    <div class="mb-3">
+                        <label for="description" class="control-label">Description</label>
+                        {{-- il name e l'id importanti per dati server e cerco di rispettare nomi colonne db --}}
+                        <textarea  class="form-control" id="description" name="description" placeholder="" rows="6"></textarea>
+                    
+                    </div>
+
+
+                    <div class="mb-3">
+                        <label for="type" class="control-label">Type</label>
+                        {{-- il name e l'id importanti per dati server e cerco di rispettare nomi colonne db --}}
+                        <select class="form-control" name="type" id="type">
+                            <option value="comic book">Comic Book</option>
+                            <option value="graphic novel">Graphic Novel</option>
+                        </select>
+                    
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="image" class="control-label">Url Image</label>
+                        {{-- il name e l'id importanti per dati server e cerco di rispettare nomi colonne db --}}
+                        <input type="text" class="form-control" id="image" name="image" placeholder=""> 
+                    
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Create Comic</button>
+
+
                 </form>
             </div>
         
